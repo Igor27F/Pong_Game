32493a8c96e643f443b01a800477a96ac47022e0
Igor Rocha <igor.2703@hotmail.com>
2023-09-30 02:48:45 -0300

    codigo comentado

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+#declaração de constantes
 PADDLE_SIZE = 200
 PADDLE_THICKNESS = 10
 LEFT_PADDLE_OFFSET = 20
@@ -13,7 +14,9 @@ SCREEN_SIZE = (800, 600)
 pygame.font.init()
 POINTS_FONT = pygame.font.SysFont('Courier', 50)
 
+#classe da raquete utilizada tanto para a raquete da esquerda como para a da direita
 class Paddle:
+    #construtor da classe
     def __init__(self, size, thickness, offset, speed):
         self.size = size
         self.thickness = thickness
@@ -25,18 +28,23 @@ class Paddle:
         self.rect.y = SCREEN_SIZE[1] / 2 - size / 2
         self.speed = speed
 
+    #função para mover a raquete
     def move(self, x, y):
         self.rect.x += x * self.speed
         self.rect.y += y * self.speed
 
+    #função para atualizar a posição da raquete
     def update(self):
         self.rect.y += self.speed
         self.rect.clamp_ip(pygame.Rect(0, 0, SCREEN_SIZE[0], SCREEN_SIZE[1]))
 
+    #função para desenhar a raquete
     def draw(self, screen):
         screen.blit(self.surface, self.rect)
 
+#classe da bola
 class Ball:
+    #construtor da classe
     def __init__(self, size):
         self.size = size
         self.surface = pygame.Surface((size, size))
@@ -45,11 +53,13 @@ class Ball:
         self.speed = [0, 0]
         self.reset()
 
+    #função para resetar a bola
     def reset(self):
         self.rect.x = SCREEN_SIZE[0] / 2 - self.size / 2
         self.rect.y = SCREEN_SIZE[1] / 2 - self.size / 2
         self.speed = [random.choice([-1, 1]) * BALL_SPEED, random.choice([-1, 1]) * BALL_SPEED]
 
+    #função para atualizar a posição da bola
     def update(self):
         self.rect.x += self.speed[0]
         self.rect.y += self.speed[1]
@@ -58,27 +68,35 @@ class Ball:
         if self.rect.left <= 0 or self.rect.right >= SCREEN_SIZE[0]:
             self.speed[0] *= -1
 
+    #função para verificar se a bola colidiu com a raquete
     def collide(self, paddle):
         if self.rect.colliderect(paddle.rect):
             self.speed[0] *= -1
 
+    #função para desenhar a bola
     def draw(self, screen):
         screen.blit(self.surface, self.rect)
 
+#classe para o placar
 class Score:
+    #construtor da classe
     def __init__(self):
         self.score = [0, 0]
 
+    #função para atualizar o placar
     def update(self, player):
         self.score[player] += 1
 
+    #função para resetar o placar
     def reset(self):
         self.score = [0, 0]
 
+    #função para desenhar o placar
     def draw(self, screen):
         text = POINTS_FONT.render(str(self.score[0]) + ' x ' + str(self.score[1]), True, (255, 255, 255))
         screen.blit(text, (SCREEN_SIZE[0] / 2 - text.get_width() / 2, 10))
 
+#função para desenhar os elementos na tela
 def draw(screen, left_paddle, right_paddle, ball, score):
     screen.fill((0, 0, 0))
     left_paddle.draw(screen)
@@ -87,7 +105,9 @@ def draw(screen, left_paddle, right_paddle, ball, score):
     score.draw(screen)
     pygame.display.flip()
 
+#função principal
 def main():
+    #inicialização das variáveis
     left_paddle = Paddle(PADDLE_SIZE, PADDLE_THICKNESS, LEFT_PADDLE_OFFSET, 0)
     right_paddle = Paddle(PADDLE_SIZE, PADDLE_THICKNESS, RIGHT_PADDLE_OFFSET, 0)
     ball = Ball(BALL_SIZE)
@@ -96,12 +116,17 @@ def main():
     screen = pygame.display.set_mode(SCREEN_SIZE)
     clock = pygame.time.Clock()
     pygame.display.set_caption("Pong")
+
+    #loop principal
     running = True
     while running:
         clock.tick(30)
+        #verificação de eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            #verificação de teclas pressionadas
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     left_paddle.speed = -PADDLE_SPEED
@@ -121,6 +146,7 @@ def main():
                 if event.key == pygame.K_DOWN:
                     right_paddle.speed = 0
 
+        #atualização dos elementos
         ball.collide(left_paddle)
         ball.collide(right_paddle)
         left_paddle.update()
@@ -132,10 +158,12 @@ def main():
         if ball.rect.right >= SCREEN_SIZE[0]:
             score.update(0)
             ball.reset()
+            
+        #desenho dos elementos
         draw(screen, left_paddle, right_paddle, ball, score)
 
 
-
+#execução do programa
 if __name__ == '__main__':
     main()
 
